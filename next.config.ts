25144747e5f19da60652254e8e4d4028cf595306
gorkamorka13 <your.email@example.com>
2026-02-02 20:45:26 +0100

    bug

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['recharts'],
-    },
+    serverExternalPackages: ['recharts'],
 };
 
 export default nextConfig;
